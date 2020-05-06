a15134dbd7a9b91166de9eb89f08c3ca21250fde
KenKwok <kenkinming2002@gmail.com>
2020-05-06 22:58:36 +0800

    Add warning if bass is not installed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,10 @@
 #!/bin/fish
 
-bass source /etc/profile
+if type -q bass
+  bass source /etc/profile
+else
+  echo (set_color red)"Warning:"(set_color white)" Bass is not installed. /etc/profile will not be sourced"
+end
 
 set PATH ~/.local/bin $PATH
 
