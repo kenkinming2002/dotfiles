6b84897d2af0bb043413ebcaa500626393d85e31
KenKwok <kenkinming2002@gmail.com>
2021-10-08 21:32:16 +0800

    Replace use of fish_variables and universal variables

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,3 +1,33 @@
+# Color Scheme
+set fish_color_autosuggestion FFC473
+set fish_color_cancel \x2dr
+set fish_color_command FF9400
+set fish_color_comment A63100
+set fish_color_cwd green
+set fish_color_cwd_root red
+set fish_color_end FF4C00
+set fish_color_error FFDD73
+set fish_color_escape bryellow\x1e\x2d\x2dbold
+set fish_color_history_current \x2d\x2dbold
+set fish_color_host normal
+set fish_color_host_remote yellow
+set fish_color_match \x2d\x2dbackground\x3dbrblue
+set fish_color_normal normal
+set fish_color_operator bryellow
+set fish_color_param FFC000
+set fish_color_quote BF9C30
+set fish_color_redirection BF5B30
+set fish_color_search_match bryellow\x1e\x2d\x2dbackground\x3dbrblack
+set fish_color_selection white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
+set fish_color_status red
+set fish_color_user brgreen
+set fish_color_valid_path \x2d\x2dunderline
+set fish_greeting Welcome\x20to\x20fish\x2c\x20the\x20friendly\x20interactive\x20shell
+set fish_pager_color_completion \x1d
+set fish_pager_color_description B3A06D\x1eyellow
+set fish_pager_color_prefix white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
+set fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
+
 ## GIT
 # git branch color
 set __fish_git_prompt_color_branch yellow
@@ -54,7 +84,7 @@ function fish_prompt
     echo -n (set_color normal)(__fish_git_prompt)
 
 		# job info
-    echo 
+    echo
     for job in (jobs)
         echo (set_color brown)$job
     end
