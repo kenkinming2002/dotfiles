ed96f21c7bad83d6b308313408f823b3087ef2c3
KenKwok <kenkinming2002@gmail.com>
2022-02-23 16:59:04 +0800

    Change cargo home to .cargo and add .cargo/bin to PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,7 @@ else
   echo (set_color red)"Warning:"(set_color white)" Bass is not installed. /etc/profile will not be sourced"
 end
 
-set PATH ~/.local/bin $PATH
+set PATH ~/.local/bin ~/.cargo/bin $PATH
 
 export XDG_RUNTIME_DIR=/tmp/users/kenkwok
 mkdir -p $XDG_RUNTIME_DIR
