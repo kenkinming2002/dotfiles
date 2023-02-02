fbc4a16328906e52a8e4898148857772312231f3
KenKwok <kenkinming2002@gmail.com>
2023-02-02 16:38:21 +0800

    Update nvim-tree.lua

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -1,14 +1,6 @@
 local nvim_tree = require('nvim-tree')
 nvim_tree.setup {
   view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    preserve_window_proportions = false,
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
     mappings = {
       custom_only = false,
       list = {
