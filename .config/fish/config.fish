635995405ec605c37ccbedd7ea50ad19672e3280
KenKwok <kenkinming2002@gmail.com>
2023-10-09 17:04:06 +0800

    Remove usage of bass to source /etc/profile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,4 @@
 #!/bin/fish
-
-if type -q bass
-  bass source /etc/profile
-else
-  echo (set_color red)"Warning:"(set_color white)" Bass is not installed. /etc/profile will not be sourced"
-end
-
 set PATH ~/.local/bin ~/.cargo/bin $PATH
 
 export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
